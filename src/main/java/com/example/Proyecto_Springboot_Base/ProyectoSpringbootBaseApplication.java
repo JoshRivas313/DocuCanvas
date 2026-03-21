7142c45a9c0acc5ed56417f625ffb72d5427955e
Omar-Cc <U22227800@utp.edu.pe>
2026-03-21 10:11:32 +0100

    Configuracion Base SpringBoot

---
+++ src/main/java/com/example/Proyecto_Springboot_Base/ProyectoSpringbootBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Springboot_Base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringbootBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringbootBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Proyecto_Springboot_Base/ProyectoSpringbootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Springboot_Base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringbootBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
