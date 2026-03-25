1798fac5d2676d6870f516cca5f0d78d5f922d03
Jose <hurtadorivasj123@gmail.com>
2026-03-25 13:13:32 -0500

    test: Implementando tests unitarios QuestionService con Mockito + Testcontainers configurado

--- src/test/java/com/docucanvas/DocuCanvasApplicationTests.java
+++ src/test/java/com/docucanvas/DocuCanvasApplicationTests.java
@@ -1,10 +1,8 @@
 package com.docucanvas;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DocuCanvasApplicationTests {
+class DocuCanvasApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
